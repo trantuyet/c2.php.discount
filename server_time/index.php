6e35f5a8bf0bb64c680a081baea775ab3038e626
trandau2k2 <trantuyet.fightforfreedom@gmail.com>
2020-10-14 16:40:48 +0700

    display current time with format day-month-year hour:minutes:sections

---
+++ server_time/index.php
@@ -0,0 +1,11 @@
+
+<?php date_default_timezone_set('Asia/Ho_Chi_Minh') ?>
+
+<html>
+<head>
+    <title>Current Time</title>
+</head>
+<body>
+<h3 style="color: purple">  <?php echo "Now is : " .date('d-M-Y h:m:s'); ?> </h3>
+</body>
+</html>
